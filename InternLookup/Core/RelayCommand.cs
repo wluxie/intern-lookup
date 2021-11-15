76add9500dd64ce5ea0ac302c3834e46a1904ed4
wluxie <49565505+wluxie@users.noreply.github.com>
2021-11-14 16:39:22 -0800

    Added Viewpoint Switches & Update Design

---
+++ InternLookup/Core/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace InternLookup.Core
+{
+    class RelayCommand : ICommand
+    {
+        private Action<object> varExecute;
+        private Func<object, bool> varCanExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            varExecute = execute;
+            varCanExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return varCanExecute == null || varCanExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            varExecute(parameter);
+        }
+    }
+}
